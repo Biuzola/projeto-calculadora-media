90cd6a930372d5678d4124d3c462be4c3fa9e657
Pedro <pedro.sant.mon@hmail.com>
2024-12-12 20:02:59 -0300

    update 2

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function atualizarTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('meida-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
